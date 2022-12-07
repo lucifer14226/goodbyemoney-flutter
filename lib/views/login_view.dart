7cbeaa42933f65b42a4cd2cc56bd2a86e0db880b
atharva2031 <atharvad467@gmail.com>
2022-12-07 13:03:20 +0530

    Add Loading Screens

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -31,112 +31,128 @@ class _RegisterViewState extends State<LoginView> {
     super.dispose();
   }
 
+  bool isLoading = false;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.white,
-        body: SingleChildScrollView(
-          child: Container(
-            padding: const EdgeInsets.all(50),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-              
-                Form(
-                  child: Container(
-                    padding: const EdgeInsets.symmetric(vertical: 110),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        TextFormField(
-                          decoration: const InputDecoration(
-                            prefixIcon: Icon(Icons.person_outline_outlined),
-                            labelText: "E-Mail",
-                            hintText: "E-Mail",
-                            border: OutlineInputBorder(),
+        body: isLoading
+            ? Center(
+                child: CircularProgressIndicator(),
+              )
+            : SingleChildScrollView(
+                child: Container(
+                  padding: const EdgeInsets.all(50),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Form(
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(vertical: 110),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              TextFormField(
+                                decoration: const InputDecoration(
+                                  prefixIcon:
+                                      Icon(Icons.person_outline_outlined),
+                                  labelText: "E-Mail",
+                                  hintText: "E-Mail",
+                                  border: OutlineInputBorder(),
+                                ),
+                                controller: _email,
+                                obscureText: false,
+                                autocorrect: false,
+                                keyboardType: TextInputType.emailAddress,
+                              ),
+                              TextFormField(
+                                decoration: const InputDecoration(
+                                  prefixIcon: Icon(Icons.fingerprint),
+                                  labelText: "Password",
+                                  hintText: "Password",
+                                  border: OutlineInputBorder(),
+                                ),
+                                controller: _password,
+                                obscureText: true,
+                                autocorrect: false,
+                              ),
+                              const SizedBox(height: 30),
+                              SizedBox(
+                                width: double.infinity,
+                                child: ElevatedButton(
+                                  onPressed: () async {
+                                    final email = _email.text;
+                                    final password = _password.text;
+                                    setState(() {
+                                      isLoading = true;
+                                    });
+                                    await Firebase.initializeApp(
+                                      options: DefaultFirebaseOptions
+                                          .currentPlatform,
+                                    );
+                                    try {
+                                      await AuthServices.firebase().logIn(
+                                        email: email,
+                                        password: password,
+                                      );
+                                      setState(() {
+                                        isLoading = false;
+                                      });
+                                      final user =
+                                          AuthServices.firebase().currentUser;
+                                      if (user?.isEmailVerified ?? false) {
+                                        // ignore: use_build_context_synchronously
+                                        Navigator.of(context)
+                                            .pushNamedAndRemoveUntil(
+                                          tabRoute,
+                                          (route) => false,
+                                        );
+                                      } else {
+                                        // ignore: use_build_context_synchronously
+                                        Navigator.of(context)
+                                            .pushNamedAndRemoveUntil(
+                                          verifyemail,
+                                          (route) => false,
+                                        );
+                                      }
+                                    } on WrongPasswordException {
+                                      ("Wrong Password");
+                                    } on UserNotFoundException {
+                                      ("user not found");
+                                    }
+                                  },
+                                  style: ElevatedButton.styleFrom(
+                                    backgroundColor: Colors
+                                        .black, // background (button) color
+                                    foregroundColor:
+                                        Colors.white, // foreground (text) color
+                                  ),
+                                  child: const Text("Login"),
+                                ),
+                              ),
+                              Align(
+                                alignment: Alignment.center,
+                                child: TextButton(
+                                  onPressed: () {
+                                    Navigator.of(context)
+                                        .pushNamedAndRemoveUntil(
+                                            registerRoutes, (route) => false);
+                                  },
+                                  child: const Text(
+                                      "Don't have an account? Register here!!"),
+                                ),
+                              ),
+                            ],
                           ),
-                          controller: _email,
-                          obscureText: false,
-                          autocorrect: false,
-                          keyboardType: TextInputType.emailAddress,
                         ),
-                        TextFormField(
-                          decoration: const InputDecoration(
-                            prefixIcon: Icon(Icons.fingerprint),
-                            labelText: "Password",
-                            hintText: "Password",
-                            border: OutlineInputBorder(),
-                          ),
-                          controller: _password,
-                          obscureText: true,
-                          autocorrect: false,
-                        ),
-                        const SizedBox(height: 30),
-                        SizedBox(
-                          width: double.infinity,
-                          child: ElevatedButton(
-                            onPressed: () async {
-                              final email = _email.text;
-                              final password = _password.text;
-                              await Firebase.initializeApp(
-                                options: DefaultFirebaseOptions.currentPlatform,
-                              );
-                              try {
-                                await AuthServices.firebase().logIn(
-                                  email: email,
-                                  password: password,
-                                );
-                                final user =
-                                    AuthServices.firebase().currentUser;
-                                if (user?.isEmailVerified ?? false) {
-                                  // ignore: use_build_context_synchronously
-                                  Navigator.of(context).pushNamedAndRemoveUntil(
-                                    tabRoute,
-                                    (route) => false,
-                                  );
-                                } else {
-                                  // ignore: use_build_context_synchronously
-                                  Navigator.of(context).pushNamedAndRemoveUntil(
-                                    verifyemail,
-                                    (route) => false,
-                                  );
-                                }
-                              } on WrongPasswordException {
-                                ("Wrong Password");
-                              } on UserNotFoundException {
-                                ("user not found");
-                              }
-                            },
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor:
-                                  Colors.black, // background (button) color
-                              foregroundColor:
-                                  Colors.white, // foreground (text) color
-                            ),
-                            child: const Text("Login"),
-                          ),
-                        ),
-                        Align(
-                          alignment: Alignment.center,
-                          child: TextButton(
-                            onPressed: () {
-                              Navigator.of(context).pushNamedAndRemoveUntil(
-                                  registerRoutes, (route) => false);
-                            },
-                            child: const Text(
-                                "Don't have an account? Register here!!"),
-                          ),
-                        ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
                 ),
-              ],
-            ),
-          ),
-        ),
+              ),
       ),
     );
   }
--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -30,102 +30,120 @@ class _RegisterViewState extends State<RegisterView> {
     super.dispose();
   }
 
+  bool isLoading = false;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.white,
-        body: SingleChildScrollView(
-          child: Container(
-            padding: const EdgeInsets.all(50),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Form(
-                  child: Container(
-                    padding: const EdgeInsets.symmetric(vertical: 110),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        TextFormField(
-                          decoration: const InputDecoration(
-                            prefixIcon: Icon(Icons.person_outline_outlined),
-                            labelText: "Enter your Email-Id",
-                            hintText: "Enter your Email-Id",
-                            border: OutlineInputBorder(),
+        body: isLoading
+            ? Center(
+                child: CircularProgressIndicator(),
+              )
+            : SingleChildScrollView(
+                child: Container(
+                  padding: const EdgeInsets.all(50),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Form(
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(vertical: 110),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              TextFormField(
+                                decoration: const InputDecoration(
+                                  prefixIcon:
+                                      Icon(Icons.person_outline_outlined),
+                                  labelText: "Enter your Email-Id",
+                                  hintText: "Enter your Email-Id",
+                                  border: OutlineInputBorder(),
+                                ),
+                                controller: _email,
+                                obscureText: false,
+                                autocorrect: false,
+                                keyboardType: TextInputType.emailAddress,
+                              ),
+                              TextFormField(
+                                decoration: const InputDecoration(
+                                  prefixIcon: Icon(Icons.fingerprint),
+                                  labelText: "Enter your Password",
+                                  hintText: "Enter your Password",
+                                  border: OutlineInputBorder(),
+                                ),
+                                controller: _password,
+                                obscureText: true,
+                                autocorrect: false,
+                              ),
+                              const SizedBox(height: 30),
+                              SizedBox(
+                                width: double.infinity,
+                                child: ElevatedButton(
+                                  onPressed: () async {
+                                    final email = _email.text;
+                                    final password = _password.text;
+                                    setState(() {
+                                      isLoading = true;
+                                    });
+                                    await Firebase.initializeApp(
+                                      options: DefaultFirebaseOptions
+                                          .currentPlatform,
+                                    );
+                                    try {
+                                      await AuthServices.firebase().createUser(
+                                        email: email,
+                                        password: password,
+                                      );
+                                      setState(() {
+                                        isLoading = false;
+                                      });
+                                      AuthServices.firebase()
+                                          .sendEmailVerification();
+                                      // ignore: use_build_context_synchronously
+                                      Navigator.of(context)
+                                          .pushNamed(verifyemail);
+                                    } on WeakPasswordException {
+                                      print("Weak Password");
+                                    } on InvalidEmailException {
+                                      print("Invalid email");
+                                    } on EmailAlreadyInUseException {
+                                      print("Email Already in use");
+                                    }
+                                  },
+                                  style: ElevatedButton.styleFrom(
+                                    backgroundColor: Colors
+                                        .black, // background (button) color
+                                    foregroundColor:
+                                        Colors.white, // foreground (text) color
+                                  ),
+                                  child: const Text("Register"),
+                                ),
+                              ),
+                              Align(
+                                alignment: Alignment.center,
+                                child: TextButton(
+                                  onPressed: () {
+                                    Navigator.of(context)
+                                        .pushNamedAndRemoveUntil(
+                                      loginRoutes,
+                                      (route) => false,
+                                    );
+                                  },
+                                  child: const Text(
+                                      "Already Registerd? Login Here"),
+                                ),
+                              ),
+                            ],
                           ),
-                          controller: _email,
-                          obscureText: false,
-                          autocorrect: false,
-                          keyboardType: TextInputType.emailAddress,
                         ),
-                        TextFormField(
-                          decoration: const InputDecoration(
-                            prefixIcon: Icon(Icons.fingerprint),
-                            labelText: "Enter your Password",
-                            hintText: "Enter your Password",
-                            border: OutlineInputBorder(),
-                          ),
-                          controller: _password,
-                          obscureText: true,
-                          autocorrect: false,
-                        ),
-                        const SizedBox(height: 30),
-                        SizedBox(
-                          width: double.infinity,
-                          child: ElevatedButton(
-                            onPressed: () async {
-                              final email = _email.text;
-                              final password = _password.text;
-                              await Firebase.initializeApp(
-                                options: DefaultFirebaseOptions.currentPlatform,
-                              );
-                              try {
-                                await AuthServices.firebase().createUser(
-                                  email: email,
-                                  password: password,
-                                );
-                                AuthServices.firebase().sendEmailVerification();
-                                // ignore: use_build_context_synchronously
-                                Navigator.of(context).pushNamed(verifyemail);
-                              } on WeakPasswordException {
-                                print("Weak Password");
-                              } on InvalidEmailException {
-                                print("Invalid email");
-                              } on EmailAlreadyInUseException {
-                                print("Email Already in use");
-                              }
-                            },
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor:
-                                  Colors.black, // background (button) color
-                              foregroundColor:
-                                  Colors.white, // foreground (text) color
-                            ),
-                            child: const Text("Register"),
-                          ),
-                        ),
-                        Align(
-                          alignment: Alignment.center,
-                          child: TextButton(
-                            onPressed: () {
-                              Navigator.of(context).pushNamedAndRemoveUntil(
-                                loginRoutes,
-                                (route) => false,
-                              );
-                            },
-                            child: const Text("Already Registerd? Login Here"),
-                          ),
-                        ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
                 ),
-              ],
-            ),
-          ),
-        ),
+              ),
       ),
     );
   }
